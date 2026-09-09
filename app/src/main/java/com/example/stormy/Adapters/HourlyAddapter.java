fa1386c1e3cd38c58ef1bcec7d98690e43a98690
taimur2000 <152456058+taimur2000@users.noreply.github.com>
2026-09-09 15:14:48 -0400

    Update HourlyAddapter.java

--- app/src/main/java/com/example/stormy/Adapters/HourlyAddapter.java
+++ app/src/main/java/com/example/stormy/Adapters/HourlyAddapter.java
@@ -15,7 +15,7 @@ import com.example.stormy.weather.Hour;
 
 import java.util.List;
 
-// Created by Taha Siddiqui 2020-04-05
+// Created by 2020-04-05
 public class HourlyAddapter extends RecyclerView.Adapter<HourlyAddapter.ViewHolder> {
 
     private List<Hour> hours;
